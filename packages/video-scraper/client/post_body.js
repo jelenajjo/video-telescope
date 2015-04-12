24de0bf2cedb008bdb33e4f78f475ca53a7a5faf
Delgermurun <delgermurun.p@nmtec.co>
2015-04-12 09:37:20 +0900

    remove ready from post body

--- packages/video-scraper/client/post_body.js
+++ packages/video-scraper/client/post_body.js
@@ -1,31 +1,14 @@
-Template.video_post_body.onCreated(function() {
-  this.isReady = new ReactiveVar(false);
-});
-
 Template.video_post_body.onRendered(function() {
   var data = Template.currentData();
-  var self = this;
 
   if (!data.originUrl || !data.videoLocation || data.videoLocation === 's3') {
-    self.isReady.set(true);
     return;
   }
 
-  Meteor.call('updateVideoUrl', data._id, function (error, res) {
+  Meteor.call('updateVideoUrl', data._id, function (error) {
     if (error) {
       console.log(error);
-    } else if (res) {
-      if (res.videoUrl) {
-        data.videoUrl = res.videoUrl;
-      }
     }
-
-    self.isReady.set(true);
+    return;
   });
-});
-
-Template.video_post_body.helpers({
-  isReady: function() {
-    return Template.instance().isReady.get();
-  }
 });
