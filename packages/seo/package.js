bd21040ce520902994b9c124d3d45ec89715858c
Delgermurun <delgermurun.p@nmtec.co>
2015-04-20 07:52:58 +0900

    video schema

--- packages/seo/package.js
+++ packages/seo/package.js
@@ -4,9 +4,26 @@ Package.onUse(function(api) {
   api.use([
     'meteorhacks:ssr@2.1.1',
     'meteorhacks:picker@1.0.2',
+    'momentjs:moment',
+    'slug',
   ], 'server');
 
   api.addFiles([
+    'private/views/common/css.html',
+    'private/views/common/layout.html',
+    'private/views/common/open-graph.html',
+    'private/views/nav/nav.html',
+    'private/views/posts/modules/post-author.html',
+    'private/views/posts/modules/post-content.html',
+    'private/views/posts/modules/post-info.html',
+    'private/views/posts/post-body.html',
+    'private/views/posts/post-item.html',
+    'private/views/posts/post-page.html',
+    'private/views/main.html'
+    ], 'server',{isAsset:true});
+
+  api.addFiles([
     'server/router.js',
+    'server/templates.js',
   ], ['server']);
 });
--- packages/seo/server/router.js
+++ packages/seo/server/router.js
@@ -2,16 +2,44 @@ var seoPicker = Picker.filter(function(req) {
     return /_escaped_fragment_/.test(req.url);
 });
 
-seoPicker.route('/', function(params, req, res) {
-  var posts = Posts.find();
-  res.end('Hello from server');
-  return;
+function openGraphMetaProperties(post){
+  var og = {
+    type: 'video',
+    url: Router.routes.post_page_by_slug.url(post),
+    title: post.title,
+    description: post.body,
+    site_name: Settings.get('title'),
+  };
 
-  var html = SSR.render('layout', {
-    //css: css,
-    template: "home",
-    data: {posts: posts}
+  if (post.thumbnailUrl) {
+    if(post.thumbnailUrl.indexOf('//')===0){
+      post.thumbnailUrl='http:'+post.thumbnailUrl;
+    }
+    og.image = post.thumbnailUrl;
+  }
+
+  return og;
+}
+
+seoPicker.route('/p/:slug', function(params, req, res) {
+  var post=Posts.findOne({slug: params.slug});
+  if (!post) {
+    res.writeHead(404,  {
+      "Content-Type": "text/html; charset=UTF-8"
+    });
+    res.end('Not found.');
+    return;
+  }
+
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=UTF-8'
   });
+
+  var dataContext=_.extend(post,{
+    og:openGraphMetaProperties(post),
+    yield:'postPage'
+  });
+  var html = SSR.render('main', dataContext);
 
   res.end(html);
 });
